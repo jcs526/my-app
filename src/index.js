a0c21701d183c3b3b653e579d30fec0d1ae5e5a6
Chansol Jeong <mutongzang@gmail.com>
2023-07-25 23:51:02 +0900

    Component와 Props

--- src/index.js
+++ src/index.js
@@ -6,12 +6,69 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+function Welcome(props) {
+  return <h1>Hello,{props.name}</h1>
+}
+
+class WelcomeClass extends React.Component {
+  render() {
+    return <h1>Hello,{this.props.name}</h1>
+  }
+}
+
+const formatDate = (date) => {
+  return new Date(date).toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+ return <div className="UserInfo">
+    <Avatar user={props.user} />
+    <div className="UserInfo-name">
+      {props.user.name}
+    </div>
+  </div>
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+const Comment = (props) => {
+  return (<div className="Comment">
+    <UserInfo user={props.author} />
+    <div className="Comment-text">
+      {props.text}
+    </div>
+    <div className="Comment-date">
+      {formatDate(props.date)}
+    </div>
+  </div>)
+}
 const tick = () => {
   const element = (
     <div>
-      <h1>Hello, world!</h1>
+      <Welcome name="function" />
+      <WelcomeClass name="class" />
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      <App/>
+      <App />
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author} />
     </div>
   );
   root.render(element);
