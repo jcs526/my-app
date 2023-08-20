44e737ea0168d4294160407daaeddcd6f0d6d192
Chansol Jeong <mutongzang@gmail.com>
2023-08-20 21:17:57 +0900

    개선사항 2번 수정

--- src/ticTacToc/Board.js
+++ src/ticTacToc/Board.js
@@ -36,6 +36,20 @@ export default function Board({ xIsNext, squares, onPlay }) {
     return null;
   }
 
+  const drawBoard = new Array(9).fill(null).map((v, i) => i).reduce((result, value, index) => {
+    if (index % 3 === 0) {
+      result.push([value]);
+    } else {
+      result[Math.floor(index / 3)].push(value);
+    }
+    return result;
+  }, []).map((v, i) => {
+    return (<div className="board-row" key={i}>
+      {v.map((v2,i2)=>{return <Square value={squares[v2]} onSquareClick={() => handleClick(v2)}></Square>})}
+    </div>)
+  })
+
+
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -45,20 +59,6 @@ export default function Board({ xIsNext, squares, onPlay }) {
   }
   return (<>
     <div className="status">{status}</div>
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
-    </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
-    </div>
+    {drawBoard}
   </>)
 }
